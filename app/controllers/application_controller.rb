726cbf91f1acf7e493417e448a4f50c2980f26a3
Ayan Saha <ayan.saha91@gmail.com>
2015-05-04 10:55:13 +0530

    deleted unnecessary stuffs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,23 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-<<<<<<< HEAD
+
 private
 helper_method :current_user
 def current_user
   if session["access_token"]!= nil
        @current_user= session["access_token"] 
-       
+      
     end
-    
-=======
-  helper_method :current_user
- 
-  private
- 
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
->>>>>>> ccaeed13cb169915fe90c446bb71f39b86c9df21
   end
 end
 
--- app/controllers/repo_controller.rb
+++ app/controllers/repo_controller.rb
@@ -1,39 +1,23 @@
 class RepoController < ApplicationController
-<<<<<<< HEAD
- skip_before_filter :verify_authenticity_token, :only => [:analyse]
-def index
-@code=params[:code]
-@name=Octokit::Client.new(:access_token => session[:access_token])
-     # @name=@name.login
-      # puts @name.login
-
-end
-def analyse
- puts "HI"
- url=params[:r]
- puts url
- 
-=======
 skip_before_action :verify_authenticity_token, :only => [:analyse]
 def index
+@name=Octokit::Client.new(:access_token => session[:access_token])
 end
  def analyse
 
        puts "HI"
       url=params[:r]
        puts url
->>>>>>> ccaeed13cb169915fe90c446bb71f39b86c9df21
+
    if url=~ /^https?:\/\/github.com(\/*)(\/*)/
      user_name = url.split('/')[3]
      repo_name = url.split('/')[4]
      repo_name = repo_name.split('.')[0]
      puts user_name
      puts repo_name
-<<<<<<< HEAD
      octokit_config
      @user=Octokit.user user_name
      @repos=Octokit.repositories(user_name)
-puts "contributors"
      @contribs=Octokit::Client.new.contributors("#{user_name}/#{repo_name}")
       
    end
@@ -74,31 +58,4 @@ private
           end
       end
    end
-=======
-# client = Octokit::Client.new \
-  # :client_id     => "23b7ff218482e1dda8fe",
-  # :client_secret => "f514e21364160d4cd16caf4965f0eeaf1e933fc7"
- #    #@user=Octokit.user 'AyanSaha'
-  #   puts "Listing all repositories of #{user_name}"
-    @user=Octokit.user user_name
-    @repos=Octokit.repositories(user_name)
-  #   @repos.each do |repo|
-  #        puts repo.name
-  #     end
-   
-   end
- end
-def create
-  auth = request.env["omniauth.auth"]
-  user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
-  session[:user_id] = user.id
-  redirect_to root_url, :notice => "Signed in!"
-
-end
-def destroy
- session[:user_id] = nil
- redirect_to root_url, :notice => "Signed out!"
-end
-
->>>>>>> ccaeed13cb169915fe90c446bb71f39b86c9df21
 end
